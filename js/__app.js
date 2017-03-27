76e35579b6619dbdfd8b1a368bbbf47a0d885f00
Unknown <thomas.egain@gmail.com>
2017-03-27 23:54:56 +0200

    Test

---
+++ js/__app.js
@@ -0,0 +1,8 @@
+var API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxx',
+	SESSION_ID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+
+
+/**
+* Main app
+*/
+angular.module('moviesList', ['ngRoute', 'angular-toArrayFilter']);
