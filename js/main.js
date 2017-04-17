3f7e73c58116aef1008fced42d996affd03b1f9e
unknown <thomas.egain@gmail.com>
2017-04-17 14:02:22 +0200

    Recover app.js file

--- js/main.js
+++ js/main.js
@@ -529,15 +529,18 @@ angular.module('angular-toArrayFilter', [])
     }
 
 })(window, angular, undefined);
-var API_KEY = 'c50f2b116ba0aa39381424dd31c38204',
-	SESSION_ID = '822890688b0113cdff5547399562bab9d8f1ff3a';
-
-
-/**
-* Main app
-*/
-angular.module('moviesList', ['ngRoute', 'angular-toArrayFilter']);
-
+//(function() {
+	
+	var API_KEY = 'c50f2b116ba0aa39381424dd31c38204',
+		SESSION_ID = '822890688b0113cdff5547399562bab9d8f1ff3a';
+	
+	
+	/**
+	* Main app
+	*/
+	angular.module('moviesList', ['ngRoute', 'angular-toArrayFilter']);
+	
+//})();
 angular
 .module('moviesList')
 .controller('ConfigController', ['$scope', '$http', function ($scope, $http) {
